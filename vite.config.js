9104cb24eee483d577ba07459379c0989d5d90d0
SureshNec <suresh.grdr@gmail.com>
2024-01-09 00:13:56 +0530

    commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/react-bootstrap/react_bootstrap_scss/",
+  base: "/react_bootstrap_scss/",
   plugins: [react()],
 });
